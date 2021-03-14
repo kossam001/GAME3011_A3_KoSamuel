c6a852b19034762e9354ac9479d78526ef609d85
kossam001 <56322486+kossam001@users.noreply.github.com>
2021-03-13 20:51:11 -0500

    Using Assignment2 as a starting point

---
+++ Assets/Scripts/Match3Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Match3Game : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    public UnityEvent Interact;
+
+    public void OnUse()
+    {
+        Interact.Invoke();
+    }
+}
