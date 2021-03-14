6b7da9b540c2e0ad3112c997ed10014470d04be0
kossam001 <56322486+kossam001@users.noreply.github.com>
2021-03-14 18:17:22 -0400

    Can find match 3s

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,8 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BoardState
+{
+    SELECTION,
+    SHUFFLING,
+    CLEARING
+}
+
 public class Board : MonoBehaviour
 {
+    private static Board instance;
+    public static Board Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+
+        else
+            instance = this;
+        
+    }
+
+    public BoardState boardState = BoardState.SELECTION;
+
     public Transform rightBound;
     public Transform leftBound;
     public Transform bottomBound;
@@ -14,9 +36,14 @@ public class Board : MonoBehaviour
     public GameObject tileTemplate;
     public List<Sprite> tileSprites;
 
+    public List<Tile> shiftedTiles;
+    public List<Tile> allMatches;
+
     // Start is called before the first frame update
     void Start()
     {
+        allMatches = new List<Tile>();
+
         SetBounds(rows, columns);
         CreateTiles(0.5f, 0.5f);
     }
@@ -40,6 +67,98 @@ public class Board : MonoBehaviour
                 GameObject tile = Instantiate(tileTemplate, new Vector3(x + xOffset, y + yOffset), Quaternion.identity);
                 tile.GetComponent<SpriteRenderer>().sprite = tileSprites[Random.Range(0, tileSprites.Count)];
             }
+        }
+    }
+
+    private IEnumerator FindMatches()
+    {
+        foreach (Tile tile in shiftedTiles)
+        {
+            Vector2[] checkHorizontalDirections = { Vector2.left, Vector2.right }; // Up not necessary
+            Vector2[] checkVerticalDirections = { Vector2.up, Vector2.down }; // Up not necessary
+
+            yield return StartCoroutine(CheckInDirections(checkHorizontalDirections, tile));
+            yield return StartCoroutine(CheckInDirections(checkVerticalDirections, tile));
+
+            tile.shifted = false; // Checked
+        }
+
+        for (int i = 0; i < allMatches.Count; i++)
+        {
+            if (allMatches[i] != null)
+                Destroy(allMatches[i].gameObject);
+        }
+
+        allMatches.Clear();
+
+
+        shiftedTiles.Clear();
+
+        boardState = BoardState.SHUFFLING;
+    }
+
+    private IEnumerator CheckInDirections(Vector2[] directions, Tile tile)
+    {
+        List<Tile> matches = new List<Tile>();
+        
+        matches.Add(tile);
+
+        Tile matchingTile = tile;
+
+        // Find all matching tiles in the row left and right of the tile
+        foreach (Vector2 direction in directions)
+        {
+            while (matchingTile != null)
+            {
+                matchingTile = CheckInDirection(matchingTile, direction);
+
+                if (matchingTile != null)
+                    matches.Add(matchingTile);
+
+                yield return null;
+            }
+
+            // Go back to starting tile and check opposite direction
+            matchingTile = tile;
+        }
+
+        if (matches.Count < 3)
+            matches.Clear();
+
+        else
+            allMatches.AddRange(matches);
+    }
+
+    private Tile CheckInDirection(Tile tile, Vector2 direction)
+    {
+        RaycastHit2D[] hit = Physics2D.RaycastAll(tile.transform.position, direction, 1.0f);
+
+        foreach (RaycastHit2D result in hit)
+        {
+            if (result.collider.gameObject.GetComponent<Tile>() != null)
+            {
+                if (result.collider.gameObject.GetComponent<Tile>().sprite == tile.GetComponent<Tile>().sprite && result.distance > 0.0f)
+                {
+                    return result.collider.gameObject.GetComponent<Tile>();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private void Update()
+    {
+        switch(boardState)
+        {
+            case BoardState.SELECTION:
+                break;
+            case BoardState.SHUFFLING:
+                break;
+            case BoardState.CLEARING:
+                boardState = BoardState.SHUFFLING;
+                StartCoroutine(FindMatches());
+                break;
         }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,4 +5,39 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public Sprite sprite;
+
+    public bool shifted;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>().sprite;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Find shifting tiles
+        // Tiles are shifting if they are moving
+        // Mark tiles that moving so they aren't checked again
+        if (GetComponent<Rigidbody2D>().velocity.magnitude > 0.1f 
+            && Board.Instance.boardState == BoardState.SHUFFLING 
+            && !shifted)
+        {
+            Board.Instance.shiftedTiles.Add(this);
+            shifted = true;
+        }
+    }
+
+    private void ClearTilesInDirection(GameObject tile, Vector2 direction, List<GameObject> matchList)
+    {
+        RaycastHit2D hit2D = Physics2D.Raycast(tile.transform.position, direction);
+
+        if (hit2D.collider.gameObject.GetComponent<Tile>().sprite == tile.GetComponent<Tile>().sprite)
+        {
+
+        }
+    }
+
+    private void Update()
+    {
+    }
 }
