145845cf8e9dc28878c28ac1b4b2faf9872d19da
kossam001 <56322486+kossam001@users.noreply.github.com>
2021-03-16 17:35:51 -0400

    Adjacent Tiles are selectable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 mousePosition;
 
+    public Tile selectedTile;
+    public Tile previouslySelectedTile;
+    public ContactPoint2D[] adjacentTiles = new ContactPoint2D[4];
+
     public void OnUse()
     {
         Interact.Invoke();
@@ -20,10 +24,40 @@ public class PlayerController : MonoBehaviour
         Vector2 mousePosition2D = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, -Camera.main.transform.position.z));
         RaycastHit2D hit = Physics2D.Raycast(mousePosition2D, Vector2.zero);
 
-        if (hit.collider != null)
+        if (hit.collider != null && hit.collider.gameObject.CompareTag("Tile"))
         {
-            Board.Instance.boardState = BoardState.SHUFFLING;
-            Destroy(hit.collider.gameObject);
+            //Board.Instance.boardState = BoardState.SHUFFLING;
+            //Destroy(hit.collider.gameObject);
+
+            if (selectedTile == null)
+            {
+                selectedTile = hit.collider.gameObject.GetComponent<Tile>();
+                selectedTile.gameObject.GetComponent<SpriteRenderer>().color *= 0.5f;
+
+                hit.collider.GetContacts(adjacentTiles);
+            }
+            else
+            {
+                ContactPoint2D[] contacts = new ContactPoint2D[4];
+                hit.collider.GetContacts(contacts);
+
+                bool adjacentSelected = false;
+
+                foreach (ContactPoint2D contact in adjacentTiles)
+                {
+                    if (ReferenceEquals(contact.collider, hit.collider))
+                    {
+                        contact.collider.gameObject.GetComponent<SpriteRenderer>().color *= 0.1f;
+                        adjacentSelected = true;
+                    }
+                }
+
+                if (!adjacentSelected)
+                {
+                    selectedTile.gameObject.GetComponent<SpriteRenderer>().color *= 2.0f;
+                    selectedTile = null;
+                }
+            }
         }
     }
 
